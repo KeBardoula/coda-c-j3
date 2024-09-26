47bc95aad3aa147736b1c799b676288200a2c859
Kévin Bardoula <kevin@MacBook-Pro-de-Kevin.local>
2024-09-26 10:32:28 +0200

    modification

--- ex-2/main.c
+++ ex-2/main.c
@@ -3,20 +3,20 @@
 
 int main(){
 	/* code */
-	int operation;
+	char operation;
 
-	printf("Choisissez une opération : 1 = - ou 2 = + ou 3 = * ou 4 = / ou 5 = module\n");
-	scanf("%d", &operation);
+	printf("Choisissez une opération : +, -, *, /, %%:\n");
+	scanf("%c", &operation);
 
-	if (operation == 1){
+	if (operation == '-'){
 		printf("Votre opération est une soustraction !\n");
-	} else if (operation == 2){
+	} else if (operation == '+'){
 		printf("Votre opération est une addition !\n");
-	} else if (operation == 3){
+	} else if (operation == '*'){
 		printf("Votre opération est une multiplication !\n");
-	}else if (operation == 4){
+	}else if (operation == '/'){
 		printf("Votre opération est une division !\n");
-	}else if (operation == 5){
+	}else if (operation == '%'){
 		printf("Votre opération est un module !\n");
 	} else {
 		printf("Erreur : vous n'avez pas entrer d'opération !\n");
